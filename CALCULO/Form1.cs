65840a20d9024f0ab38cbad09c9a3cc94f7120d5
Matheus Santos <68879085+matheussantosguedes00@users.noreply.github.com>
2021-11-21 19:00:39 -0300

    UP

---
+++ CALCULO/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace execicio4b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            a = Convert.ToInt32(textBox1.Text);
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Número vazio", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else 
+            {
+                int val=0;
+                for (int i = 1; i <= a; i++)
+                {
+                    if (a % i == 0)
+                    {
+                        val++;
+                    }
+                }
+                if (val == 2)
+                {
+                    MessageBox.Show(a+" : "+"E primo", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show(a + " : " + "Não e primo", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a;
+            a = Convert.ToInt32(textBox1.Text);
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Número vazio", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                int  fatorial;
+                
+
+                fatorial = a;
+                for (int i = a - 1; i >= 1; i--)
+                {
+                   
+
+                    fatorial = fatorial * i;
+
+                  
+                }
+                MessageBox.Show("O fatorial do número " + a + " é " + fatorial, "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        
+    }
+ }
+
